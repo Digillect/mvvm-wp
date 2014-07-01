35ab57dac37a53f98a9c6af0e36f6c6a12dd1ef4
Alexander Stavnichiy <alexander.stavnichiy@actis.ru>
2014-07-01 17:10:35 +0400

    Version up to 2.2.6

--- src/AssemblySharedInfo.cs
+++ src/AssemblySharedInfo.cs
@@ -47,7 +47,7 @@ internal static class AssemblyInfo
 {
 	public const string Major = "2";
 	public const string Minor = "2";
-	public const string Revision = "5";
+	public const string Revision = "6";
 	public const string BuildNumber = "0";
 	public const string Suffix = "";
 
