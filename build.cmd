d8184666ea597c5e9d36bd32958b4119b06c2dde
Andrew Nefedkin <andrew.nefedkin@actis.ru>
2015-12-08 14:42:29 +0300

    Switched to Digillect.Build.Tools 2.0.
    Introduced per-project packages.config files.
    Upgraded all NuGet packages.

--- build.cmd
+++ build.cmd
@@ -1,14 +1,28 @@
 @echo off
 
 setlocal enableextensions
-set PATH=%~dp0\tools;%PATH%
-set BuildTargets=%~dp0\packages\Digillect.Build.Tools.1.2.5\tools\Build.targets
 set EnableNuGetPackageRestore=true
 
-if not exist "%BuildTargets%" (
-	nuget.exe install -o "%~dp0\packages" "%~dp0\.nuget\packages.config"
+pushd %~dp0
+
+call :ResolveNuGet nuget.exe || exit /b %ERRORLEVEL%
+
+if exist .nuget\packages.config (
+	echo Restoring packages from %~dp0.nuget\packages.config
+	"%NuGetExe%" restore .nuget\packages.config -PackagesDirectory packages -Verbosity quiet -NonInteractive || exit /b %ERRORLEVEL%
 )
 
-if not errorlevel 1 (
-	%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj %*
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /nologo /v:m /p:NuGetExecutable="%NuGetExe%" %*
+
+popd
+goto :EOF
+
+:ResolveNuGet
+if exist %CD%\.nuget\%1 (
+	set NuGetExe=%CD%\.nuget\%1
+	goto :EOF
 )
+set NuGetExe=%~$PATH:1
+if not "%NuGetExe%"=="" goto :EOF
+echo NuGet.Exe was not found either in .nuget subfolder or PATH
+exit /b 1
--- src/AssemblySharedInfo.cs
+++ src/AssemblySharedInfo.cs
@@ -1,15 +1,15 @@
 ﻿#region Copyright (c) 2011-2014 Gregory Nickonov and Andrew Nefedkin (Actis® Wunderman)
 // Copyright (c) 2011-2014 Gregory Nickonov and Andrew Nefedkin (Actis® Wunderman).
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
 // without restriction, including without limitation the rights to use, copy, modify, merge,
 // publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
 // to whom the Software is furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all copies or
 // substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -25,29 +25,28 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 #if DEBUG
-
-[assembly: AssemblyConfiguration( "Debug" )]
+[assembly: AssemblyConfiguration("Debug")]
 #else
 [assembly: AssemblyConfiguration("Retail")]
 #endif
 
-[assembly: AssemblyCompany( "Actis Systems" )]
-[assembly: AssemblyProduct( "Windows Phone adaptor for Digillect� Model-View-ViewModel framework." )]
-[assembly: AssemblyCopyright( "� 2011-2014 Actis Systems. All rights reserved." )]
-[assembly: AssemblyTrademark( "Digillect is a registered trademark of Actis Systems." )]
-[assembly: AssemblyVersion( AssemblyInfo.Version )]
-[assembly: AssemblyFileVersion( AssemblyInfo.FileVersion )]
-[assembly: AssemblyInformationalVersion( AssemblyInfo.ProductVersion )]
-[assembly: CLSCompliant( false )]
-[assembly: ComVisible( false )]
-[assembly: NeutralResourcesLanguage( "en-US" )]
-[assembly: SatelliteContractVersion( AssemblyInfo.SatelliteContractVersion )]
+[assembly: AssemblyCompany("Actis Wunderman")]
+[assembly: AssemblyProduct("Windows Phone adaptor for Digillect® Model-View-ViewModel framework.")]
+[assembly: AssemblyCopyright("© 2011-2015 Gregory Nickonov and Andrew Nefedkin (Actis® Wunderman). All rights reserved.")]
+[assembly: AssemblyTrademark("Digillect is a registered trademark of Actis Wunderman.")]
+[assembly: AssemblyVersion(AssemblyInfo.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfo.FileVersion)]
+[assembly: AssemblyInformationalVersion(AssemblyInfo.ProductVersion)]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: SatelliteContractVersion(AssemblyInfo.SatelliteContractVersion)]
 
 internal static class AssemblyInfo
 {
 	public const string Major = "2";
 	public const string Minor = "2";
-	public const string Revision = "7";
+	public const string Revision = "8";
 	public const string BuildNumber = "0";
 	public const string Suffix = "";
 
